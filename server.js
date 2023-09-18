7aa6021d52ba2f86491bfa5694b00a85c34332a9
Scav <42140533+Scavix@users.noreply.github.com>
2023-09-18 03:36:01 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const path = require('path');
+const cors = require('cors');
+
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(cors({
+    origin: 'https://morse-prolog-u4tg.onrender.com/'
+}));
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
